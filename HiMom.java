59d768bc8264a1777b35133a732d3e6989fad0dc
tippyj7 <jconell27@gmail.com>
2018-03-02 06:48:29 -0500

    Main files?

---
+++ HiMom.java
@@ -0,0 +1,23 @@
+package testinggithub;
+
+public class HiMom {
+
+    private int tacos;
+
+    public HiMom() {
+        tacos = 999;
+    }
+
+    public HiMom(int tacos) {
+        this.tacos = tacos;
+    }
+
+    public int getTacos() {
+        return tacos;
+    }
+
+    public void setTacos(int tacos) {
+        this.tacos = tacos;
+    }
+
+}
---
+++ TestingGitHub.java
@@ -0,0 +1,23 @@
+
+package testinggithub;
+
+/*
+Kinda obvious, I am learning how to use github
+*/
+
+import java.util.*;
+
+public class TestingGitHub {
+
+   
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+        
+        System.out.println("FYM");
+        
+        
+        
+    }
+    
+}
